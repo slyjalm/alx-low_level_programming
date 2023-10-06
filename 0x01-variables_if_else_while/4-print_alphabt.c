eaa423d3528a0fb0a1c8184f8b9a60fbf0e5b1ac
slyjalme <kiendesylvia8@gmail.com>
2023-10-07 00:31:22 +0300

    print all aplhabets except  two

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	char c;
+	for (c = 'a'; c <= 'z'; c++) {
+        if (c != 'q' && c != 'e') {
+            putchar(c);
+        }
+}
+         putchar ('\n');
+	 return 0;
+	 }
