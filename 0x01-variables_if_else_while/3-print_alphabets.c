cabd26b2787544191ab0e48168c836ca3be2bdd5
slyjalme <kiendesylvia8@gmail.com>
2023-10-08 22:09:56 +0300

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,13 +2,12 @@
 
 int main(void)
 {
-       char c;																								for (c = 'a'; c <= 'z'; ++c)
+       char c;
+       
+       for (c = 'a'; c <= 'z' c++)
        {
         putchar(c);
-    }
-
-   
-    for (c = 'A'; c <= 'Z'; ++c)
+    }													for (c = 'A'; c <= 'Z'; ++c)
     {
         putchar(c);
     }
