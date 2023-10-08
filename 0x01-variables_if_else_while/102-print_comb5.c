f21a8826be7cb8c452d8162bc45a8c036030f508
slyjalme <kiendesylvia8@gmail.com>
2023-10-08 19:28:05 +0300

    prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,13 +20,13 @@ int main(void)
 	{
 	putchar((a / 10) + '0');
 	putchar((a % 10) + '0');
-	putchar(' ');
+	putchar(' '); 
 	putchar((b / 10) + '0');
 	putchar((b % 10) + '0');
 	if (a == 98 && b == 99)
 	continue;
 	putchar(','); 
-	putchar(' '); 
+	putchar(' ');
 	}
 	}
 	putchar('\n');
