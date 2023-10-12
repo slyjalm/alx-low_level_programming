ac625bf0897c6e9b16e6f337b924db17d35cffc5
slyjalme <kiendesylvia8@gmail.com>
2023-10-12 05:58:08 +0300

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,8 +8,7 @@
 void jack_bauer(void)
 
 {
-	int a, b, c, d;
-	
+	int a, b, c, d;	
 	for (a = 0; a <= 2; a++)
 	{
 	for (b = 0; b <= 9; b++)
@@ -25,7 +24,7 @@ void jack_bauer(void)
 	_putchar(58);
 	_putchar(c + '0');
 	_putchar(d + '0');
-	_putchar('\n');;
+	_putchar('\n');
 	}
 	}
 	}
