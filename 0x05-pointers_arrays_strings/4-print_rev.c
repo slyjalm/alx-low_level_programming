42e9be22fbbf5c5be6ff85b639dc7cf1f119421f
slyjalme <kiendesylvia8@gmail.com>
2023-10-16 22:00:05 +0300

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @a: string
+ * Return: 0
+ */
+void print_rev(char *a)
+{
+	int longi = 0;
+	int 0;
+
+	while (*a != '\0')
+	{
+		longi++;
+		a++;
+	}
+	a--;
+	for (o = longi; o > 0; o--)
+	{
+		putchar(*a);
+		a--;
+	}
+	putchar('\n');
+}
