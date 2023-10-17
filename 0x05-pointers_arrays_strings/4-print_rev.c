72d97783f738bdc78edc90bc108c71324f1f5e77
slyjalme <kiendesylvia8@gmail.com>
2023-10-17 16:58:55 +0300

    Fixed code failure

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,16 +10,14 @@ void print_rev(char *s)
 	int c = 0;
 	int i = 0;
 
-	while (*s != '\0')
+	while (s[c] != '\0')
 	{
 		c++;
-		s++;
 	}
 	
-	for (i = c; i > 0; i--)
+	for (i = c - 1; i >= 0; i--)
 	{
-		putchar(*s);
-		s--;
+		putchar(s[i]);
 	}
 	putchar('\n');
 }
