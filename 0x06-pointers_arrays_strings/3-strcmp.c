684537a276bb619dd2023bf5689e8a289adc9628
slyjalme <kiendesylvia8@gmail.com>
2023-10-19 04:22:28 +0300

    removed a trailing white space and added a description for s1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,7 +2,7 @@
 
 /**
  * _strcmp - compares two strings
- * @s1/: input
+ * @s1: input
  * @s2: input
  * Return: s1[i] - s2[i]
  */
@@ -12,11 +12,10 @@ int _strcmp(char *s1, char *s2)
 	int i;
 
 	i = 0;
-	while ( s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
-		{				    
-		return (s1[i] - s2[i]);
+		{												return (s1[i] - s2[i]);
 		}
 		i++;
 	}
