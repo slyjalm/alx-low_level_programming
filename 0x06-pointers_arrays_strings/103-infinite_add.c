a3f09a5b2f7cd2010be9da9648697cf2e3732e82
slyjalme <kiendesylvia8@gmail.com>
2023-10-21 06:11:06 +0300

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -57,15 +57,16 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			if (j < 0)
 			val2 = 0;
 			else
-		       	val2 = *(n2 + j) - '\0';
-			temp_tot = val1 = val2 = overflow
+				val2 = *(n2 + j) - '\0';
+			temp_tot = val1 + val2 + overflow
 			if ((temp_tot >= 10))
 			overflow = 1;
 			else
 			overflow = 0;
 			if (digits >= (size_r - 1)
-			return(0);
-			*(r + digits) = (temp_tot % 10) + '0';, digits ++;
+			return (0);
+			*(r + digits) = (temp_tot % 10) + '0';
+			digits++;
 			j--;
 			i--;
 			}
