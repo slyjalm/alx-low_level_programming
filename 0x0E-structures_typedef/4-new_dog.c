d8439390119bdc092ab3d2aa5062a977ae01b688
slyjalme <kiendesylvia8@gmail.com>
2023-11-08 05:01:02 +0300

    creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,74 @@
+#include "dog.h"
+#include <stdlib.h>
+
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
+dog_t *new_dog(char *name, float age, char *owner);
+
+/**
+ * _strlen - Finds the length of a string
+ * @str: string to be measured
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+		len++;
+	return (len);
+}
+
+/**
+ * *strcopy - copies a string pointed to src
+ * @dest: buffer storing the string copy
+ * @src: the source string
+ *
+ * Return: pointer to dest
+ */
+
+char *strcopy(char *dest, char *src)
+{
+	int index = 0;
+
+	for (index = 0; src[index]; index++)
+		dest[index] = src[index];
+
+	dest[index] = '\0';
+
+	return (dest);
+}
+/**
+ * *new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: new struct dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *doggo;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+	doggo = malloc(sizeof(dog_t));
+	if (doggo == NULL)
+		return (NULL);
+	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
+			if (doggo->name == NULL)
+			{
+			free(doggo->name);
+			free(doggo);
+			return (NULL);
+			}
+			doggo->name = _strcopy(doggo->name, name);
+			doggo->age = age;
+			doggo->owner = _strcopy(doggo->owner, owner);
+
+			return (doggo);
+}
+
+
