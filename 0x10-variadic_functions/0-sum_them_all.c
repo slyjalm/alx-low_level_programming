b9d086d37d340e85c429e3f93ed4587f5027129d
slyjalme <kiendesylvia8@gmail.com>
2023-11-10 05:03:28 +0300

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,7 +9,7 @@
  * Otherwise - the sum of all parameters
  *
  */
-int sum_them_all(const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i, sum = 0;
