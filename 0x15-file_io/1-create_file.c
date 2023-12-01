589298be9fe759dc0d402cd12ac3cc035cde8488
slyjalme <kiendesylvia8@gmail.com>
2023-12-02 01:01:51 +0300

    changed line 27

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,8 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (kletters = 0; text_content[kletters]; kletters++);
+	for (kletters = 0; text_content[kletters]; kletters++)
+		;
 	rwr = write(file_d, text_content, kletters);
 	if (rwr == -1)
 		return (-1);
