d67e73f548e0e595ec918a24e07f3bfc29cb47c0
slyjalme <kiendesylvia8@gmail.com>
2023-12-15 16:51:03 +0300

    run betty on the file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -11,7 +11,7 @@ void myStartupFun(void) __attribute__((constructor));
 */
 void myStartupFun(void)
 {
-   printf("You're beat! and yet, you must allow,\n");
-   printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
