d7cc1d5203fbdad8d82d4ea498df38c1979fb9ef
slyjalme <kiendesylvia8@gmail.com>
2023-12-15 16:49:00 +0300

    changed the entire comment line

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
-void first(void) _attribute_((constructor));
 /**
- * first - prints a sentence before the function is executed
- *
- */
-void first(void)
+* myStartupFun - Apply the constructor attribute to myStartupFun()
+*/
+void myStartupFun(void) __attribute__((constructor));
+
+
+/**
+* myStartupFun - implementation of myStartupFun
+*/
+void myStartupFun(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+   printf("You're beat! and yet, you must allow,\n");
+   printf("I bore my house upon my back!\n");
 }
+
